87f670590e0dc41a75af0d1b475ce4e12fa553f9
netsammy <33105017+netsammy@users.noreply.github.com>
2026-05-13 04:40:01 +0500

    Deploy GitHub Pages build

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = "alien-base-defense-v1";
+const APP_SHELL = ["./", "manifest.webmanifest", "icon.svg", "privacy.html"];
+
+function scopedUrl(path) {
+  return new URL(path, self.registration.scope).toString();
+}
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL.map(scopedUrl))));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
